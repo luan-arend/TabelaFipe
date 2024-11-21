a38c302c34de91b179ad193d4ed8fd936e8f1344
luan-arend <luan00luchie@hotmail.com>
2024-11-21 15:40:58 -0300

    Ajustando opções do usuário

--- src/main/java/br/com/luarend/TabelaFipe/principal/Principal.java
+++ src/main/java/br/com/luarend/TabelaFipe/principal/Principal.java
@@ -4,6 +4,7 @@ import br.com.luarend.TabelaFipe.model.DadosVeiculo;
 import br.com.luarend.TabelaFipe.service.HttpRequests;
 import br.com.luarend.TabelaFipe.service.JsonDataMapper;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Principal {
@@ -11,15 +12,27 @@ public class Principal {
     private HttpRequests httpRequests = new HttpRequests();
     private JsonDataMapper jsonDataMapper = new JsonDataMapper();
 
-    private final String URL = "https://parallelum.com.br/fipe/api/v1/";
+    private final String URL_BASE = "https://parallelum.com.br/fipe/api/v1/";
 
     public void exibeMenu(){
         System.out.println("Digite o que deseja buscar: " +
                 "Carros / Caminhao / Moto");
 
         var tipoVeiculo = scanner.nextLine();
+        String endereco;
 
-        var json = httpRequests.sendRequest(URL + tipoVeiculo + "/marcas/");
+        if (tipoVeiculo.toLowerCase().contains("carr")) {
+            endereco = URL_BASE + "carros/marcas/";
+        } else if (tipoVeiculo.toLowerCase().contains("caminh")) {
+            endereco = URL_BASE + "caminhoes/marcas/";
+        } else if (tipoVeiculo.toLowerCase().contains("moto")) {
+            endereco = URL_BASE + "motos/marcas/";
+        } else {
+            System.out.println("Opção inválida, tente novamente!");
+            return;
+        }
+
+        var json = httpRequests.sendRequest(endereco);
         var dadosVeiculos = jsonDataMapper.convertJsonToList(json, DadosVeiculo.class);
         System.out.println(dadosVeiculos);
 
